d29587fcb5b555bf5e3bfe9918bb206c978b83f3
Anton Lobanov <lobanovvv@mail.ru>
2020-10-27 02:51:56 +0300

    work version

---
+++ test_add_group_unittest.py
@@ -0,0 +1,59 @@
+from selenium import webdriver
+from random import shuffle
+import time, unittest
+
+class test_add_group(unittest.TestCase):
+
+    def setUp(self):
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(3)
+        self.wd.set_window_size(1920,1080)
+        self.wd.get('http://localhost/addressbook')
+
+    def test_add_group(self):
+        #LOGIN
+        #Enter login
+        time.sleep(1)
+        el = self.wd.find_element_by_css_selector('input:nth-of-type(1)')
+        el.send_keys('admin')
+
+        #Enter password
+        el = self.wd.find_element_by_css_selector('input:nth-of-type(2)')
+        el.send_keys('secret')
+
+        #Click button Login
+        el = self.wd.find_element_by_css_selector('input[type="submit"]')
+        el.click()
+
+
+        #GROUP
+        #Jump to group tab
+        el = self.wd.find_element_by_link_text('groups')
+        el.click()
+
+        #Create new group
+        el = self.wd.find_element_by_css_selector('input[name="new"]:nth-of-type(1)')
+        el.click()
+
+        #Enter value in fields
+        el_name = [
+            'input[name="group_name"]',
+            'textarea[name="group_header"]',
+            'textarea[name="group_footer"]'
+        ]
+        abcdefg = ['a', 'b', 'c', 'd', 'e', 'f','g']
+
+        for i in el_name:
+            el = self.wd.find_element_by_css_selector(i)
+            shuffle(abcdefg)
+            el.send_keys(abcdefg)
+
+        #Submit
+        el = self.wd.find_element_by_css_selector("input[type=submit]")
+        el.click()
+
+        self.wd.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
